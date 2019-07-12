ab67bd78e66ca58dcc5bef40481a900c9c6f2fba
Cody Shaben <cshaben87@gmail.com>
2019-07-12 19:07:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,9 +40,7 @@ def monopoly_with_fourth_tier
 	 :railroads => {
 	   :pieces => 4, 
 	   :names => {
-	     :reading_railroad => {
-	       :mortgage_value => "100",
-	     },
+	     :reading_railroad => {},
 	     :pennsylvania_railroad => {},
 	     :b_and_o_railroad => {},
 	     :shortline => {},
@@ -52,7 +50,8 @@ def monopoly_with_fourth_tier
 	     :two_pieces_owned => 50,
 	     :three_pieces_owned => 100,
 	     :four_pieces_owned => 200,
-	   }
+	   },
+	   :mortgage_value => "100"
 	 }
 }
 
